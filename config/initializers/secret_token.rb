d6ffa3793003f7e35e1c1d8544dbd557b811f575
Andrea <andrea.damico@gmail.com>
2011-08-04 11:55:58 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Toxport::Application.config.secret_token = '304def391825be8f6a19bdb3204c0096276cb0ab90817d80cd9b0d801e8f753c881a616af3d3fbcbdcea2bbb5255f2a51a7062d723e2b1c504b591ddb68e4459'
